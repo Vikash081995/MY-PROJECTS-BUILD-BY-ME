d2fbc4a6c18bb3cdce87e482f7c03c728475250e
Vikash081995 <Vikash08.Kumar1995@gmail.com>
2024-02-07 00:35:08 +0530

    refactored header

--- my-digital-brain-vite/src/App.tsx
+++ my-digital-brain-vite/src/App.tsx
@@ -9,27 +9,53 @@
 
 import Header from "./components/Header/Header";
 import HeaderBackground from "./components/HeaderBackground/HeaderBackground";
-import { Box, CssBaseline } from "@mui/material";
+import { AppBar, Box, CssBaseline } from "@mui/material";
 import React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
+const theme = createTheme({
+  palette: {
+    primary: { main: "#F7CAC9" }
+  },
+  components: {
+    MuiSvgIcon: {
+      styleOverrides: {
+        root: {
+          fontSize: "3rem" // Change this to the font size you want
+        }
+      }
+    },
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#079e49" // Change this to the color you want
+        }
+      }
+    }
+  }
+});
 const App = () => {
   return (
     <React.Fragment>
-      <CssBaseline />
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          height: "100vh",
-          width: "100vw",
-          backgroundColor: "black"
-        }}
-      >
-        <HeaderBackground />
-        <Box sx={{ marginTop: "1vh" }}>
-          <Header />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            height: "100vh",
+            width: "100vw",
+            backgroundColor: "black"
+          }}
+        >
+          <HeaderBackground />
+          <Box sx={{ marginTop: "1vh" }}>
+            <AppBar position="fixed">
+              <Header />
+            </AppBar>
+          </Box>
         </Box>
-      </Box>
+      </ThemeProvider>
     </React.Fragment>
   );
 };
--- my-digital-brain-vite/src/components/Header/Header.tsx
+++ my-digital-brain-vite/src/components/Header/Header.tsx
@@ -1,46 +1,16 @@
-import {
-  AppBar,
-  Toolbar,
-  Container
-} from "@mui/material";
+import { Toolbar, Container } from "@mui/material";
 import Logo from "../Logo/Logo";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import AvatarImage from "../Avatar/Avatar";
 
-const theme = createTheme({
-  palette: {
-    primary: { main: "#F7CAC9" }
-  },
-  components: {
-    MuiSvgIcon: {
-      styleOverrides: {
-        root: {
-          fontSize: "3rem" // Change this to the font size you want
-        }
-      }
-    },
-    MuiAppBar: {
-      styleOverrides: {
-        root: {
-          backgroundColor: "#079e49" // Change this to the color you want
-        }
-      }
-    }
-  }
-});
+import AvatarImage from "../Avatar/Avatar";
 
 const Header = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <AppBar position="fixed">
-        <Container sx={{ maxWidth: "x1" }}>
-          <Toolbar disableGutters>
-            <Logo />
-            <AvatarImage />
-          </Toolbar>
-        </Container>
-      </AppBar>
-    </ThemeProvider>
+    <Container sx={{ maxWidth: "x1" }}>
+      <Toolbar disableGutters>
+        <Logo />
+        <AvatarImage />
+      </Toolbar>
+    </Container>
   );
 };
 
